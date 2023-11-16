42dfa0c8c2e67ea6ee97f62376155ba60d102ab6
Oksana <oksa.osidach@gmail.com>
2023-11-16 17:47:42 +0100

    start writing a heap sort

---
+++ heap_sort.h
@@ -0,0 +1,28 @@
+#ifndef PROJECT_1_1_HEAP_SORT_H
+#define PROJECT_1_1_HEAP_SORT_H
+
+// left child = 2*i + 1
+// right child = 2*i + 2
+// parent = (j-1)/2
+
+void building_a_heap(std::vector<int>::iterator iter_begin, std::vector<int>::iterator iter_end){
+    std::vector<int> heap;
+    int i_index, parent_index;
+    for (std::vector<int>::iterator j; j != iter_begin; j++){
+        heap.push_back(*j);
+        i_index = heap.size() - 1 ;
+        parent_index = (i_index - 1) / 2;
+        std::vector<int>::iterator i = heap.begin() + i_index;
+        std::vector<int>::iterator parent = heap.begin() + parent_index;
+        if(*i < *parent){
+            std::swap(*i, *parent);
+            std::swap(i, parent);
+        }
+    }
+
+}
+
+void heap_sort(std::vector<int>::iterator iter_begin, std::vector<int>::iterator iter_end){
+
+}
+#endif //PROJECT_1_1_HEAP_SORT_H
