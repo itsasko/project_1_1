4021d6b646d649a533b39bd61a8d90e28af4c5dd
Oksana <oksa.osidach@gmail.com>
2023-11-29 16:59:15 +0100

    radix_sort correcting (at least it doesn't cause memory issues)

--- radix_sort.h
+++ radix_sort.h
@@ -2,56 +2,61 @@
 #define PROJECT_1_1_RADIX_SORT_H
 
 
-static std::vector<int> counting_sort(std::vector<int>::iterator iter_begin, std::vector<int>::iterator iter_end, int digit_to_sort, int max_digits){
-    std::vector<int> pos;
-    std::vector<int> sorted;
-    for(int i = 0; i < 10; i++) pos.push_back(0);
-    for(int i = 0; i < std::distance(iter_begin, iter_end); i++) sorted.push_back(0);
-    for(auto i = iter_begin; i <= iter_end; i++){
-        int aux = *i, digit;
+static std::vector<int> counting_sort(std::vector<int> &xs, int digit_to_sort, int max_digits){
+    std::vector<int>::iterator iter_begin = xs.begin();
+    std::vector<int>::iterator iter_end = xs.end() - 1;
+    std::vector<int> pos(10, 0);
+    std::vector<int> sorted(xs.size(), 0);
+    for(auto i : xs) {
+        int aux = i, digit;
         for(int j = 1; j <= max_digits; j++){
-            if(j == digit_to_sort) digit = aux%10;
+            if(j == digit_to_sort){
+                digit = aux%10;
+                break;
+            }
             aux /= 10;
         }
         pos[digit] += 1;
     }
     for(int i = 1; i < pos.size(); i++) pos[i] += pos[i - 1];
-    for(auto i = iter_end; i >= iter_begin; i-- ){
-        int aux = *i, digit;
-        for(int j = 0; j < max_digits; j++){
-            if(j == digit_to_sort) digit = aux%10;
+    for(auto i : xs){
+        int aux = i, digit;
+        for(int j = 1; j <= max_digits; j++){
+            if(j == digit_to_sort) {
+                digit = aux%10;
+                break;
+            }
             aux /= 10;
         }
         pos[digit] --;
-        sorted[pos[digit]] = *i;
+        sorted[pos[digit]] = i;
     }
     return sorted;
 }
 
-static int find_max(std::vector<int>::iterator iter_begin, std::vector<int>::iterator iter_end){
-    int max_value = *iter_begin;
-    iter_begin++;
-    for(auto i = iter_begin; i <= iter_end; i++){
-        if(*i > max_value) max_value = *i;
+static int find_max(std::vector<int> &xs){
+    int max_value = xs[0];
+    for(auto i  : xs){
+        if(i > max_value) max_value = i;
     }
     return max_value;
 }
 
-void radix_sort(std::vector<int>::iterator iter_begin, std::vector<int>::iterator iter_end) {
-    int max_value = find_max(iter_begin, iter_end), digits = 0;
+void radix_sort(std::vector<int> &xs) {
+    int max_value = find_max(xs), digits = 0;
     std::vector<int> rs;
     int aux = max_value;
     while (aux >= 1) {
         digits++;
         aux /= 10;
     } // number of digits in the biggest number is obtained
-    iter_begin = rs.begin();
-    iter_end = rs.end()-1;
+    int j = 0;
     for(int i = 1; i <= digits; i++){
-        rs = counting_sort(iter_begin, iter_end, i, digits);
+        rs = counting_sort(xs, i, digits);
         for(auto k : rs) std::cout << k << " ";
         std::cout << std::endl;
     }
+
 }
 
 #endif //PROJECT_1_1_RADIX_SORT_H
