3ca9e547b9dcf117f61caddb3963983f76fe31cb
Oksana <oksa.osidach@gmail.com>
2023-11-30 02:53:41 +0100

    tests file (unsuccessfully)

--- tests.h
+++ tests.h
@@ -1,15 +1,41 @@
 #ifndef PROJECT_1_1_TESTS_H
 #define PROJECT_1_1_TESTS_H
 
+#include <fstream>
 #include <vector>
 #include <algorithm>
+#include <chrono>
+#include <random>
 #include "quick_sort.h"
 #include "heap_sort.h"
 #include "insertion_sort.h"
 #include "radix_sort.h"
 
+static std::vector<int>& random_numbers_generator(int lower_bound, int upper_bound){
+    using std::chrono::nanoseconds;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    std::ofstream fout("test_data.txt");
+
+    std::uniform_int_distribution<int> dist_elements(lower_bound, upper_bound);
+    std::uniform_int_distribution<int> dist_size(0, 1000);
+    std::vector<int> random_test;
+    int test_vector_size = dist_size(gen);
+    int element;
+    for(int i = 0; i < test_vector_size; i++){
+        element = dist_elements(gen);
+        random_test.push_back(element);
+        fout << element << " ";
+    }
+    fout << std::endl;
+    fout.close();
+    return random_test;
+
+}
+
 void quick_sort_testing(){
-    std::vector<int> test{500, 446538, 31413, 232842, 385, 269, 113989};
+    std::vector<int> test(random_numbers_generator(-1000, 1000));
     std::vector<int>::iterator begin = test.begin();
     std::vector<int>::iterator end = test.end() - 1;
     std::cout << "Quick sort " << std::endl;
@@ -21,7 +47,7 @@ void quick_sort_testing(){
     for (auto i : test) std::cout << i << " ";
 }
 void heap_sort_testing(){
-    std::vector<int> test{500, 446538, 31413, 232842, 385, 269, 113989};
+    std::vector<int> test = random_numbers_generator(-1000, 1000);
     std::vector<int>::iterator begin = test.begin();
     std::vector<int>::iterator end = test.end() - 1;
     std::cout << "Heap sort " << std::endl;
@@ -33,7 +59,7 @@ void heap_sort_testing(){
     for (auto i : test) std::cout << i << " ";
 }
 void insertion_sort_testing(){
-    std::vector<int> test{500, 446538, 31413, 232842, 385, 269, 113989};
+    std::vector<int> test = random_numbers_generator(-1000, 1000);
     std::cout << "Insertion sort " << std::endl;
     std::cout << "Original vector of numbers: ";
     for (auto i : test) std::cout << i << " ";
@@ -44,7 +70,7 @@ void insertion_sort_testing(){
 }
 
 void radix_sort_testing(){
-    std::vector<int> test{500, 446538, 31413, 232842, 385, 269, 113989};
+    std::vector<int> test = random_numbers_generator(0, 1000);
     std::cout << "Radix sort " << std::endl;
     std::cout << "Original vector of numbers: ";
     for (auto i : test) std::cout << i << " ";
